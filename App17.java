2910b08d289554e765039e0093e853fab741a727
omar2432 <50786739+omar2432@users.noreply.github.com>
2023-05-07 21:38:18 +0300

    Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R)

--- App17.java
+++ App17.java
@@ -9,6 +9,8 @@ public class App17 {
     public int solution(int[] A) {
         int N = A.length;
         Arrays.sort(A);
+// maxproduct is the multiplication of the last (biggest) 3 numbers of the array
+
         int maxProduct = A[N - 1] * A[N - 2] * A[N - 3];
         if (A[0] < 0 && A[1] < 0 && A[N - 1] > 0) {
             int maxNegProduct = A[0] * A[1] * A[N - 1];
