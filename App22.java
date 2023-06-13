faeac3290ed34d9710e59789626fb2878575d29c
omar2432 <50786739+omar2432@users.noreply.github.com>
2023-06-13 22:12:34 +0300

    Count factors of given number n. Improved

--- App22.java
+++ App22.java
@@ -5,22 +5,23 @@ public class App22 {
     }
     
 
-    public int solution(int N) {
+     public int solution(int N) {
         int count = 0;
-        
+        int sqrtN = (int) Math.sqrt(N);
+
         // Iterate through numbers from 1 to sqrt(N)
-        for (int i = 1; i * i <= N; i++) {
+        for (int i = 1; i <= sqrtN; i++) {
             if (N % i == 0) {
-                // If i is a factor, count both i and its corresponding factor
-                if (i * i == N) {
-                    count++;
-                } else {
-                    count += 2;
-                }
+                // If i is a factor, count both i and its corresponding factor (N / i)
+                count += 2;
             }
+        }
+        
+        // Check if N is a perfect square, and if so, count the square root as an additional factor
+        if (sqrtN * sqrtN == N) {
+            count--;
         }
         
         return count;
     }
-
 }
